33a4a2865b6981a8fcdec2e8b08aa4e57d336a57
Bruno Rodrigues <brunorodrigues0955@gmail.com>
2021-04-11 14:43:39 -0300

    projeto anitvhub 1.0

---
+++ js/owl/setup.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function redirecionar(){
+    window.open("https://www.crunchyroll.com/pt-br/86-eighty-six");
+    
+}
+
+
+function trailer(){
+    window.open("https://www.youtube.com/watch?v=VSdS29SDvn4");
+    
+}
