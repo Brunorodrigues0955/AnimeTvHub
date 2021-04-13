28312099f2f121af233d1c93f3d94fb81ec433b3
Bruno Rodrigues <brunorodrigues0955@gmail.com>
2021-04-13 13:48:37 -0300

    1.6

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -25,3 +25,20 @@ function trailer(){
     window.open("https://www.youtube.com/watch?v=VSdS29SDvn4");
     
 }
+
+function info1(){
+    window.open("https://github.com/Brunorodrigues0955");
+    
+}
+
+function info2(){
+    window.open("https://github.com/Brunorodrigues0955/AnimeTvHub");
+    
+}
+
+function home(){
+
+    window.location.href = "index.html";
+
+}
+
